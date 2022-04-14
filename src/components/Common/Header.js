d841c0e4fbf7c25d4af352dbbfbaa1fe13129f9a
Léa Hamadouche <leahamadouche@hotmail.com>
2022-04-14 16:11:19 +0200

    Fix Header Style

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,13 +1,13 @@
-import { Grid } from '@mui/material'
+import { Grid, CardMedia } from '@mui/material'
 
 export default function Header() {
-    return (
-        <>
-            <Grid container justifyContent="center" alignItems="center">
-                <Grid item>
-                    <img src="/Pokedex Header.png" alt="" />
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <Grid container justifyContent="center" alignItems="center">
+        <Grid item sm={7} xs={10} m={5}>
+          <CardMedia component="img" image="/Pokedex Header.png" alt="" />
+        </Grid>
+      </Grid>
+    </>
+  )
 }
