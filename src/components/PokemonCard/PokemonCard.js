03b9a0dec40eb06652cc5933272b06a02423eed2
Adrian SOREAU <adrian.soreau@estiam.com>
2022-05-24 15:08:43 +0200

    style

--- src/components/PokemonCard/PokemonCard.js
+++ src/components/PokemonCard/PokemonCard.js
@@ -24,31 +24,36 @@ export default function PokemonCard({ pokemon }) {
         }}
       >
         <Grid container justifyContent="center">
-          <img id="pokemonCardImg" src={pokemon.sprites?.front_default} alt="" />
+          <img
+            id="pokemonCardImg"
+            src={pokemon.sprites?.front_default}
+            alt=""
+          />
         </Grid>
       </Grid>
       <Typography fontFamily="Roboto" className="Card__name" mt={4}>
         {pokemon.name}
       </Typography>
       <Grid container justifyContent="center">
-        {pokemon?.types && pokemon.types.map((type, id) => {
-          return (
-            <Typography
-              color="white"
-              borderRadius="3px"
-              p={1}
-              m={1}
-              key={id}
-              sx={{
-                backgroundColor: TypeColor[type.type.name],
-                display: 'inline-block',
-                textTransform: 'capitalize',
-              }}
-            >
-              {type.type.name}
-            </Typography>
-          )
-        })}
+        {pokemon?.types &&
+          pokemon.types.map((type, id) => {
+            return (
+              <Typography
+                color="white"
+                borderRadius="3px"
+                p={1}
+                m={1}
+                key={id}
+                sx={{
+                  backgroundColor: TypeColor[type.type.name],
+                  display: 'inline-block',
+                  textTransform: 'capitalize',
+                }}
+              >
+                {type.type.name}
+              </Typography>
+            )
+          })}
       </Grid>
       <Grid container justifyContent="center" mt={2}>
         <Link to={`/details/${pokemon.id}`}>
